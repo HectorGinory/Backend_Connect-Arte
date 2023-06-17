91e475c8158fdbae98b2a0becea2db682b0a0d3e
HectorGinory <hectorginorydevs@gmail.com>
2023-06-17 19:36:13 +0200

    feat: seeders created

--- app.ts
+++ app.ts
@@ -1,49 +1,77 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import routerUser from './entities/user/router.js';
-import routerVacancies from './entities/jobVacancies/router.js';
-import config from './config.js';
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import routerUser from "./entities/user/router.js";
+import routerVacancies from "./entities/jobVacancies/router.js";
+import config from "./config.js";
+import cors from "cors";
+import { seedUsers, seedVacancies } from "./core/seeder.js";
 
 export const app = express();
 
-mongoose.connect(config.DDBB!).then(()=>{
-    console.log('Connected to the database')
-}).catch((e)=>{
-    console.log('ERROR:', e)
-})
+mongoose
+  .connect(config.DDBB!)
+  .then(() => {
+    console.log("Connected to the database");
 
-const handlerError = (err:Error,req,res,next)=>{
-  console.log(err)
-    if(err.message === 'NO_TOKEN'){
-      return res.status(400).json({code:err.message,message:"Debes tener ña sesión iniciada"});
-    }
-    if(err.message === 'NO_AUTH'){
-      return res.status(401).json({code:err.message,message:"No tienes autorización para hacer esto"});
-    }
-    if(err.message === 'INFO_LEFT'){
-      return res.status(400).json({code:err.message,message:"Falta información por rellenar"});
-    }
-    if(err.message === 'NO_VACANCIE'){
-      return res.status(404).json({code:err.message,message:"No se ha encontrado esa oferta"});
-    }
-    if(err.message === 'NO_USER'){
-      return res.status(404).json({code:err.message,message:"No se ha encontrado ese usuario"});
-    }
-    
-    return res.status(500).json({code:'SERVER_ERROR', message: "Error de servidor"});    
+    // seedUsers(10).then(() => {
+    //   seedVacancies(10).then(() => {
+    //     console.log("Datos enviados");
+    //   });
+    // }).catch(e => {
+    //   console.log(e)
+    // });
+  }
+  )
+  .catch((e) => {
+    console.log("ERROR:", e);
+  });
+
+const handlerError = (err: Error, req, res, next) => {
+  console.log(err);
+  if (err.message === "NO_TOKEN") {
+    return res
+      .status(400)
+      .json({ code: err.message, message: "Debes tener ña sesión iniciada" });
+  }
+  if (err.message === "NO_AUTH") {
+    return res
+      .status(401)
+      .json({
+        code: err.message,
+        message: "No tienes autorización para hacer esto",
+      });
+  }
+  if (err.message === "INFO_LEFT") {
+    return res
+      .status(400)
+      .json({ code: err.message, message: "Falta información por rellenar" });
+  }
+  if (err.message === "NO_VACANCIE") {
+    return res
+      .status(404)
+      .json({ code: err.message, message: "No se ha encontrado esa oferta" });
+  }
+  if (err.message === "NO_USER") {
+    return res
+      .status(404)
+      .json({ code: err.message, message: "No se ha encontrado ese usuario" });
+  }
+
+  return res
+    .status(500)
+    .json({ code: "SERVER_ERROR", message: "Error de servidor" });
 };
 
 let corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 204,
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use('/user', routerUser)
-app.use('/vacancies', routerVacancies)
+app.use("/user", routerUser);
+app.use("/vacancies", routerVacancies);
 app.use(handlerError);
 app.listen(config.PORT, () => console.log(`Server up in port ${config.PORT}`));
---
+++ core/seeder.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import { faker } from "@faker-js/faker";
+import Users from "../entities/user/model.js";
+import config from "../config.js";
+import JobVacancies from "../entities/jobVacancies/model.js";
+import mongoose from "mongoose"
+
+export const seedUsers = async (count) => {
+  let users:any = [];
+  for (let i = 0; i < count; i++) {
+    const name = faker.person.firstName();
+    const newUser = new Users({
+      name: name,
+      email: faker.internet.email(),
+      password: await bcrypt.hash("Contraseña1", 1),
+      location: faker.location.city(),
+      username: name,
+    });
+    users.push(newUser);
+  }
+  return await Users.insertMany(users);
+};
+
+const jobs = ["Actor", "Actriz", "Director de cine", "Bailarina profesional", "Cantante"]
+const sector = ["Teatro", "Cine", "Baile", "Pintura", "Canto"]
+
+export const seedVacancies = async(count) => {
+    let vacancies: any = []
+    const users = await Users.find({})
+    for(let i = 0; i < count; i++) {
+       const newVacancie = new JobVacancies({
+        created_by: users[Math.round(Math.random()*(users.length - 1))],
+        charge_name: jobs[Math.round(Math.random()*(jobs.length - 1))],
+        description : faker.lorem.lines(),
+        sector: sector[Math.round(Math.random()*(sector.length - 1))],
+        last_day: faker.date.future(),
+        location: faker.location.country,
+        question_one: "¿Cuantos años de experiencia tienes en el puesto?"
+       })
+       vacancies.push(newVacancie)
+    }
+    return await JobVacancies.insertMany(vacancies)
+}
